61d9fd033644581c6c80d97a86067bdebbad46b1
Mysticpasta1 <ethana5tc@outlook.com>
2026-01-20 10:44:58 -0600

    Update OculusCompat.java

--- src/main/java/me/erykczy/colorfullighting/compat/oculus/OculusCompat.java
+++ src/main/java/me/erykczy/colorfullighting/compat/oculus/OculusCompat.java
@@ -1,5 +1,7 @@
 package me.erykczy.colorfullighting.compat.oculus;
 
+import net.minecraftforge.fml.ModList;
+
 import java.lang.reflect.Method;
 
 public class OculusCompat {
@@ -21,7 +23,7 @@ public class OculusCompat {
             }
             checked = true;
         }
-        return loaded;
+        return loaded || ModList.get().isLoaded("oculus") || ModList.get().isLoaded("iris");
     }
 
     public static boolean isShaderPackInUse() {
